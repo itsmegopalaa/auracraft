054b388eb463bacfd5bec0fb8952d1490cf183ea
Gopalaa <unknownspirite1@gmail.com>
2026-08-05 19:52:34 +0530

    Fix AddToCartButton for deployment

--- app/products/[id]/AddToCartButton.tsx
+++ app/products/[id]/AddToCartButton.tsx
@@ -17,9 +17,13 @@ export default function AddToCartButton({ product }: Props) {
   return (
     <button
       onClick={() => {
-  addToCart(product);
-  alert(`${product.name} added to cart 🛒`);
-}}
+        addToCart({
+          ...product,
+          quantity: 1,
+        });
+
+        alert(`${product.name} added to cart 🛒`);
+      }}
       className="mt-8 rounded-full bg-yellow-400 px-10 py-4 font-bold text-black hover:scale-105 transition"
     >
       Add to Cart 🛒

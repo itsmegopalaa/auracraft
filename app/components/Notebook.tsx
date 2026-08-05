f40a7887a3c2fc58bc07044133927263dce6140e
Gopalaa <unknownspirite1@gmail.com>
2026-08-05 11:42:43 +0530

    Created Hero component

---
+++ app/components/Notebook.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+export default function Notebook() {
+  return (
+    <section className="py-24 max-w-6xl mx-auto grid md:grid-cols-2 gap-12 items-center">
+      <div>
+        <h2 className="text-5xl font-bold">
+          Your Notebook.
+          <span className="text-yellow-400"> Your Identity.</span>
+        </h2>
+
+        <p className="mt-6 text-gray-400 text-lg">
+          AuraNotes transforms ordinary notebooks into personalized pieces
+          that represent your personality, dreams and creativity.
+        </p>
+
+        <button className="mt-8 bg-yellow-400 text-black px-8 py-4 rounded-full font-semibold hover:scale-105 transition">
+          Create Your AuraNote
+        </button>
+      </div>
+
+      <div className="flex justify-center">
+        <Image
+          src="/images/notebook.png"
+          alt="AuraNotes Notebook"
+          width={420}
+          height={550}
+          className="rounded-3xl shadow-2xl hover:scale-105 transition duration-500"
+        />
+      </div>
+    </section>
+  );
+}
